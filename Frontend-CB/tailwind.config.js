3385bd33affc6a13f08bf3ab84277df6b247c460
Ian Worley <ian@ianworley.xyz>
2023-09-26 08:30:32 -0500

    #8 create a away for the user to submit user credentials (#16)
    * add .gitignore
    
    * Workable Form Issue #8
    
    * Updated Issue #8
    
    * Add ui for login screen #8
    
    * add formatting for frontend
    
    * Edited inputs #8
    
    * Added Schema Validation #8
    
    ---------
    
    Co-authored-by: kylefaris <kyle.faris2021@gmail.com>

--- Frontend-CB/tailwind.config.js
+++ Frontend-CB/tailwind.config.js
@@ -1,8 +1,15 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white': '#FFFFFF',
+        'yellow': '#FFF600',
+        'grenn': '#14DF28'
+      }
+    },
   },
   plugins: [],
 };
