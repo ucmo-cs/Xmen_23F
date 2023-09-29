e773f1250fda203c7d30b52dca9fa3ed36aff346
Ian Worley <ian@ianworley.xyz>
2023-09-29 10:33:03 -0500

    I forgot add the file content

--- CB-Backend/src/main/java/edu/ucmo/cbbackend/model/ChangeRequest.java
+++ CB-Backend/src/main/java/edu/ucmo/cbbackend/model/ChangeRequest.java
@@ -1,2 +1,64 @@
-package edu.ucmo.cbbackend.model;public class ChangeRequest {
+package edu.ucmo.cbbackend.model;
+
+import jakarta.persistence.*;
+
+@Table(name = "change_requests")
+public class ChangeRequest {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Id
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @Column(nullable = false, length = 20)
+    private String title;
+
+    @Column(nullable = false, length = 80)
+    private String description;
+
+
+    ChangeRequest(){
+
+    }
+
+    public ChangeRequest(Long id, User author, String title, String description) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.description = description;
+    }
+
 }
