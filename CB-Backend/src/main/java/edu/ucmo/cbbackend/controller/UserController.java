27b97e8f4d67454f14de254f91b9e4ee50928fa4
KyleFaris <144936755+KyleFaris@users.noreply.github.com>
2023-10-20 18:55:12 -0500

    Display user info through endpoint

--- CB-Backend/src/main/java/edu/ucmo/cbbackend/controller/UserController.java
+++ CB-Backend/src/main/java/edu/ucmo/cbbackend/controller/UserController.java
@@ -11,49 +11,52 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 
 
 @RestController
 @CrossOrigin
 public class UserController {
 
- private final UserRepository userRepository;
- private final UserService userService;
+    private final UserRepository userRepository;
+    private final UserService userService;
+
     public UserController(UserRepository userRepository, UserService userService) {
         this.userRepository = userRepository;
         this.userService = userService;
     }
+
     @Operation(summary = "Get a user by id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Return a user object", content = {@Content( mediaType = "application/json",
-                    schema = @Schema(implementation = UserResponse.class))} ),
-            @ApiResponse(responseCode = "400", description = "Return a String of 'User does not exist'", content = @Content ),
+            @ApiResponse(responseCode = "200", description = "Return a user object", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = UserResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "Return a String of 'User does not exist'", content = @Content),
 
     })
     @GetMapping("/api/v1/user/{id}")
-    public ResponseEntity<?> getUserById(@PathVariable  Long id){
+    public ResponseEntity<?> getUserById(@PathVariable Long id) {
         try {
-           User user =  userRepository.findById(id).orElseThrow(() ->  new Exception("User does not exist")); //TODO: consider add error handling
+            User user = userRepository.findById(id).orElseThrow(() -> new Exception("User does not exist")); //TODO: consider add error handling
             return ResponseEntity.ok().body(new UserResponse(user));
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.toString());
         }
     }
 
     @Operation(summary = "Create a user and return the username and id but leave out the password")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Return a user object", content = {@Content( mediaType = "application/json",
-            schema = @Schema(implementation = UserRegisterRequest.class))} ),
-            @ApiResponse(responseCode = "400", description = "Return a String of 'Username is already taken'", content = @Content ),
-            @ApiResponse(responseCode = "400", description = "Return a String of 'User id must be null'" , content = @Content ),
+            @ApiResponse(responseCode = "200", description = "Return a user object", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = UserRegisterRequest.class))}),
+            @ApiResponse(responseCode = "400", description = "Return a String of 'Username is already taken'", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Return a String of 'User id must be null'", content = @Content),
     })
     @PostMapping("/api/v1/user")
-    public ResponseEntity<?> createUser( @RequestBody  User  user){
-        if (userRepository.findByUsername(user.getUsername()) != null){
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        if (userRepository.findByUsername(user.getUsername()) != null) {
             return ResponseEntity.badRequest().body("Username is already taken");
         }
         if (user.getId() != null)
@@ -62,8 +65,7 @@ public class UserController {
         try {
             userService.save(user);
             return ResponseEntity.ok().body(new UserResponse(user));
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.toString());
         }
 
@@ -77,12 +79,12 @@ public class UserController {
 
     @Operation(summary = "Delete a user by id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Return no content", content = @Content ),
-            @ApiResponse(responseCode = "400", description = "Return a String of 'User does not exist'", content = @Content ),
+            @ApiResponse(responseCode = "204", description = "Return no content", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Return a String of 'User does not exist'", content = @Content),
     })
     @DeleteMapping("/api/v1/user/{id}")
-    public ResponseEntity<?> deleteUserById(@PathVariable Long id){
-        if (userRepository.findById(id).orElse(null) == null){
+    public ResponseEntity<?> deleteUserById(@PathVariable Long id) {
+        if (userRepository.findById(id).orElse(null) == null) {
             return ResponseEntity.badRequest().body("User does not exist");
         }
         userRepository.deleteById(id);
@@ -90,26 +92,26 @@ public class UserController {
 
     }
 
-    @Operation(summary = "Update a user by id" )
+    @Operation(summary = "Update a user by id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Return a user object", content = {@Content( mediaType = "application/json",
-                    schema = @Schema(implementation = User.class))} ),
-            @ApiResponse(responseCode = "400", description = "Return a String of 'User does not exist'", content = @Content ),
+            @ApiResponse(responseCode = "200", description = "Return a user object", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = User.class))}),
+            @ApiResponse(responseCode = "400", description = "Return a String of 'User does not exist'", content = @Content),
     })
-    @PutMapping( "/api/v1/user/{id}" )
-    public ResponseEntity<?> updateUserById(@PathVariable Long id,  @RequestBody UserEditRequest user){
+    @PutMapping("/api/v1/user/{id}")
+    public ResponseEntity<?> updateUserById(@PathVariable Long id, @RequestBody UserEditRequest user) {
         User userToUpdate = userRepository.findById(id).orElse(null);
-        if (userToUpdate == null){
+        if (userToUpdate == null) {
             return ResponseEntity.badRequest().body("User does not exist"); //Todo could verify that this works
         }
 
-        if(user.getUsername() != null)
+        if (user.getUsername() != null)
             userToUpdate.setUsername(user.getUsername());
 
         if (user.getPassword() != null)
             userToUpdate.setPassword(userService.passwordEncoder(user.getPassword()));
 
-        if(user.getRoles() != null)
+        if (user.getRoles() != null)
             userToUpdate.setRoles(user.getRoles());
 
         userService.save(userToUpdate);
@@ -117,4 +119,9 @@ public class UserController {
         return ResponseEntity.ok().body(new UserResponse(userToUpdate));
     }
 
+    @GetMapping("/api/v1/user/me")
+    public User getCurrentUser(HttpServletRequest request) {
+        Principal principal = request.getUserPrincipal();
+        return (principal != null ? (userService.loadUserByUsername(principal.getName()))  : null);
+    }
 }
